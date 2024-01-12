ce72aa93caaf509091d4fca66f1914759262ed85
Harshmeet Singh <maxdiplogit@gmail.com>
2024-01-12 20:52:26 +0530

    remove vercel config

--- FavoriteTunes/settings.py
+++ FavoriteTunes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1bm0$twulv2lp$!gzqp*^*9!0qi40v%^2lulfl*gw)y)=vxm&(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
